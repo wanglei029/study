c47515ef582c3057bfdfbed920b704cefe87e1e5
wanglei029 <610213949@qq.com>
2020-05-08 00:28:03 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 移动端300ms点击延迟
+fastClick.attach(document.body)
 
 new Vue({
   router,
