188db726d596e71f9ce8563a8e1b8f8a6abb931a
wanglei029 <610213949@qq.com>
2020-05-08 22:51:58 +0800

    change

---
+++ src/components/home/Swiper.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgurl"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        loop:true
+        // Some Swiper option/callback...
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgurl:
+            "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01LwHuP72JJi0pnIXsM_!!2053469401.jpg"
+        },
+        {
+          id: "0001",
+          imgurl:
+            "https://img.alicdn.com/imgextra/i1/2053469401/O1CN01xsmWc92JJi0rHYxeh_!!2053469401.jpg"
+        }
+      ]
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper;
+    }
+  },
+  mounted() {
+    console.log("Current Swiper instance object", this.swiper);
+    this.swiper.slideTo(3, 1000, false);
+  }
+};
+</script>
+<style lang='stylus' scoped>
+.wrapper >>>.swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 41%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from 'router'
 import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import 'styles/iconfont/iconfont.css'
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 // 移动端300ms点击延迟
