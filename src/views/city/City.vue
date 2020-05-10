a1a5d91a01fb7084cbcbe3daeaf6ed69edcc6bba
wanglei029 <610213949@qq.com>
2020-05-10 15:46:19 +0800

    ajax-city

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,11 +2,12 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities='cities' :hotCities='hotCities'></city-list>
+    <city-alphabet :cities='cities'></city-alphabet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from "components/city/Header";
 import CitySearch from "components/city/Search";
 import CityList from "components/city/List";
@@ -14,12 +15,38 @@ import CityAlphabet from "components/city/Alphabet";
 
 export default {
   name: "City",
+  data(){
+    return {
+      cities:{},
+      hotCities:[]
+    }
+  },
   components: {
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
-  }
+  },
+  methods: {
+    getCityInfo(){
+      axios.get('api/city.json')
+        // .then(this.handleGetCityInfoSucc)
+        .then(res=>{
+          res=res.data
+          if(res.ret&&res.data){
+            const data=res.data
+            this.cities=data.cities
+            this.hotCities=data.hotCities
+          }
+        })
+    },
+    handleGetCityInfoSucc(res){
+      console.log(res);
+    }
+  },
+  mounted() {
+    this.getCityInfo()
+  },
 };
 </script>
 <style lang="stylus" scoped>
