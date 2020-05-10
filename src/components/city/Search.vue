790f87e3e3037e2799e12b32e3cfce86fdf7fdda
wanglei029 <610213949@qq.com>
2020-05-10 22:06:34 +0800

    city search finish

--- src/components/city/Search.vue
+++ src/components/city/Search.vue
@@ -3,68 +3,63 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <div 
-      class="search-content" 
-      ref="search"
-      v-show="keyword"
-    >
+    <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li 
-          class="search-item border-bottom" 
-          v-for="item of list" 
-          :key="item.id"
-        >{{item.name}}</li>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
         <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配的数据</li>
       </ul>
     </div>
   </div>
 </template>
 <script>
-import Bscroll from 'better-scroll'
+import Bscroll from "better-scroll";
 export default {
   name: "CitySearch",
-  props:{
-    cities:Object
+  props: {
+    cities: Object
   },
-  data(){
+  data() {
     return {
-      keyword:'',
-      list:[],
-      timer:null
-    }
+      keyword: "",
+      list: [],
+      timer: null
+    };
   },
   computed: {
-    hasNoData(){
-      return !this.list.leangth
+    hasNoData() {
+      return !this.list.leangth;
     }
   },
   // watch 监听器 监听keyword的改变
   watch: {
-    keyword(){
+    keyword() {
       // 函数节流
-      if(this.timer){
-        clearTimeout(this.timer)
+      if (this.timer) {
+        clearTimeout(this.timer);
       }
-      if(!this.keyword){
-        this.list=[]
-        return
+      if (!this.keyword) {
+        this.list = [];
+        return;
       }
-      this.timer=setTimeout(() => {
-        const result=[]
+      this.timer = setTimeout(() => {
+        const result = [];
         for (const i in this.cities) {
-          this.cities[i].forEach((value)=>{
-            if(value.spell.indexOf(this.keyword)>-1||value.name.indexOf(this.keyword)>-1){
-              result.push(value)
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value);
             }
-          })
+          });
         }
-        this.list=result
-      }, 100);//当keyword发生改变的时候 隔100毫秒在执行
-    },
+        this.list = result;
+      }, 100); //当keyword发生改变的时候 隔100毫秒在执行
+    }
   },
   mounted() {
-    this.scroll=new Bscroll(this.$refs.search)
-  },
+    this.scroll = new Bscroll(this.$refs.search);
+  }
 };
 </script>
 <style lang="stylus" scoped>
@@ -96,11 +91,12 @@ export default {
   right: 0;
   bottom: 0;
   background: #eee;
-  .search-item{
-    line-height .62rem
-    padding-left .2rem
-    background #fff
-    color #666
+
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    background: #fff;
+    color: #666;
   }
 }
 </style>
